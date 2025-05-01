a5d684626322eb2f1e99332d34ba7121a2bd6751
satyam-chhatrala <163503828+satyam-chhatrala@users.noreply.github.com>
2025-05-01 18:57:52 +0530

    Update adminProductRoutes.js

--- routes/adminProductRoutes.js
+++ routes/adminProductRoutes.js
@@ -46,11 +46,11 @@ router.post('/', upload.fields([
         if (!req.body.name || req.body.name.trim() === "") {
             return res.status(400).json({ message: 'Product name is required.' });
         }
-        if (!req.body.productType || req.body.productType.trim() === "") {
+        if (!req.body.productTypeSelect || req.body.productTypeSelect.trim() === "") {
             return res.status(400).json({ message: 'Product type selection is required.' });
         }
         
-        let finalProductType = req.body.productType.trim().toLowerCase();
+        let finalProductType = req.body.productTypeSelect.trim().toLowerCase();
         if (finalProductType === 'other') {
             if (!req.body.newProductType || req.body.newProductType.trim() === '') {
                 return res.status(400).json({ message: 'Please specify the new product type when "Other" is selected.' });
